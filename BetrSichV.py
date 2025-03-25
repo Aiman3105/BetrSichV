420551931220ca361cb14e48bbcea0e75a5a67e9
Aiman <aimanelkhamis@yahoo.de>
2025-03-25 10:42:05 +0100

    H-Sätze anstelle von Fluidgruppe

--- BetrSichV.py
+++ BetrSichV.py
@@ -1,10 +1,24 @@
 import streamlit as st
 import Funktionen_BetrSichV
 
+
+
+FG=0
 st.title("Einstufung nach BetrSichV:")
 
+st.text("H-Sätze:")
+H_1=st.checkbox("H224, H225, H226 (T_zul > T_FP & T_FP <= 55°C), "
+                "H250, H300, H310, H330", key="H_1")
+H_2=st.checkbox("H226 (T_zul < T_FP & T_FP <= 55°C), H314", key="H_2")
 
-FG=st.text_input(label="", placeholder="Fluidgruppe",  key="Fluidgruppe")
+if H_1:
+    FG=1
+if H_2:
+    FG=1.5
+
+
+
+#FG=st.text_input(label="", placeholder="Fluidgruppe",  key="Fluidgruppe")
 AZ=st.text_input(label="", placeholder="Aggregatzustand",  key="Aggregatzustand")
 PS=st.text_input(label="", placeholder="zul Druck",  key="zul Druck")
 DN=st.text_input(label="", placeholder="DN",  key="DN")
