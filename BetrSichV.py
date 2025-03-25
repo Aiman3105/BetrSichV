582716e9bc45ca38290a701b67ecc2b6493e57d8
Aiman <aimanelkhamis@yahoo.de>
2025-03-25 10:20:35 +0100

    Rohrleitungen erstellt

--- BetrSichV.py
+++ BetrSichV.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import Funktionen_BetrSichV
 
-st.title("Einstufung nach BetrSichV")
+st.title("Einstufung nach BetrSichV:")
 
 
-st.text_input(label="", placeholder="Fluidgruppe",  key="Fluidgruppe")
-st.text_input(label="", placeholder="Aggregatzustand",  key="Aggregatzustand")
-st.text_input(label="", placeholder="zul Druck",  key="zul Druck")
-st.text_input(label="", placeholder="DN",  key="DN")
+FG=st.text_input(label="", placeholder="Fluidgruppe",  key="Fluidgruppe")
+AZ=st.text_input(label="", placeholder="Aggregatzustand",  key="Aggregatzustand")
+PS=st.text_input(label="", placeholder="zul Druck",  key="zul Druck")
+DN=st.text_input(label="", placeholder="DN",  key="DN")
+
+
+
+if FG is not "" and AZ is not "" and PS is not "" and DN is not "":
+    Prüfungen_nach_Nr4, Prüfungen_nach_Nr5 = Funktionen_BetrSichV.BetrSichV_RL(int(FG), AZ, int(PS), int(DN))
+    st.text_input("Prüfungen nach Nr 4:", Prüfungen_nach_Nr4, disabled=True)
+    st.text_input("Prüfungen nach Nr 5:", Prüfungen_nach_Nr5, disabled=True)
