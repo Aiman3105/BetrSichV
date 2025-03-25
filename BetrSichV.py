685f57d0425c2cbf04df4cc26f03e023e4f32c6b
Aiman <aimanelkhamis@yahoo.de>
2025-03-25 13:13:05 +0100

    Apparate hinzugefügt

--- BetrSichV.py
+++ BetrSichV.py
@@ -6,6 +6,11 @@ import Funktionen_BetrSichV
 FG=0
 AZ=""
 H_0=0
+H_1=0
+H_2=0
+H_3=0
+FG_1=0
+FG_2=0
 st.title("Einstufung nach BetrSichV:")
 
 st.markdown("**Druckgerät:**")
@@ -15,6 +20,10 @@ with col_App:
     Apparate = st.checkbox("Apparate", key="Apparate")
 with col_RL:
     Rohrleitungen = st.checkbox("Rohrleitungen", key="Rohrleitungen")
+
+if Apparate and Rohrleitungen:
+    st.error("Bitte nur eine Checkbox unter Druckgerät auswählen")
+
 
 
 col1, col2= st.columns(2)
@@ -30,50 +39,81 @@ if gasförmig:
 if flüssig:
     AZ = "f"
 if gasförmig and flüssig:
-    st.error("Bitte nur eine Checkbox beim Aggregatzustand auswählen")
+    st.error("Bitte nur eine Checkbox unter Aggregatzustand auswählen")
 
 
 #H-Sätze
 with col2:
-    st.markdown("**H-Sätze:**")
-    if AZ=="g":
-        H_0 = st.checkbox("akut toxisch: H300, H310, H330", key="H_0")
-        H_1 = st.checkbox("H224, H225, H226 (T_zul > T_FP & T_FP <= 55°C)", key="H_1")
-    else:
-        H_1 = st.checkbox("H224, H225, H226 (T_zul > T_FP & T_FP <= 55°C), "
-                          "H250, H300, H310, H330", key="H_1")
-    H_2 = st.checkbox("H226 (T_zul < T_FP & T_FP <= 55°C), H314", key="H_2")
-    H_3 = st.checkbox("Alle anderen Medien", key="H_3")
 
-#Bestimmung Fluidgruppe für Formel
-if H_0:
-    FG = 0.5
-if H_1:
-    FG = 1
-if H_2:
-    FG = 1.5
-if H_3:
-    FG = 2
+    if Rohrleitungen:
+        st.markdown("**H-Sätze:**")
+        if AZ=="g":
+            H_0 = st.checkbox("akut toxisch: H300, H310, H330", key="H_0")
+            H_1 = st.checkbox("H224, H225, H226 (T_zul > T_FP & T_FP <= 55°C)", key="H_1")
+        else:
+            H_1 = st.checkbox("H224, H225, H226 (T_zul > T_FP & T_FP <= 55°C), "
+                              "H250, H300, H310, H330", key="H_1")
+        H_2 = st.checkbox("H226 (T_zul < T_FP & T_FP <= 55°C), H314", key="H_2")
+        H_3 = st.checkbox("Alle anderen Medien", key="H_3")
+
+        if H_0:
+            FG = 0.5
+        if H_1:
+            FG = 1
+        if H_2:
+            FG = 1.5
+        if H_3:
+            FG = 2
+
+    if Apparate:
+        st.markdown("**Fluidgruppe**")
+        FG_1 = st.checkbox("1", key="FG_1")
+        FG_2 = st.checkbox("2", key="FG_2")
+
+        if FG_1:
+            FG=1
+        if FG_2:
+            FG=2
+
+
+
+    #Bestimmung Fluidgruppe für Formel
+
 
 checked_count_H = sum([H_0, H_1, H_2, H_3])
 if checked_count_H>1:
-    st.error("Bitte nur eine Checkbox bei den H-Sätzen auswählen")
+    st.error("Bitte nur eine Checkbox unter H-Sätze auswählen")
+
+checked_count_FG = sum([FG_1, FG_2])
+if checked_count_FG>1:
+    st.error("Bitte nur eine Checkbox unter Fluidgruppe auswählen")
 
 
-#Zulässiger Druck und Nennweite
-PS=st.text_input(label="", placeholder="Zulässiger Betriebsdruck [barg]",  key="zul Druck")
+#Zulässiger Druck und Nennweite/Volumen
 if Rohrleitungen:
     DN=st.text_input(label="", placeholder="Nennweite [DN]",  key="DN")
 if Apparate:
-    V=st.text_input(label="", placeholder="Volumen [l]",  key="V")
+    V=st.text_input(label="", placeholder="Volumen [L]",  key="V")
+PS=st.text_input(label="", placeholder="Zulässiger Betriebsdruck [barg]",  key="zul Druck")
+
 
 
 #Einstufung Rohrleitungen
 col_Eintufung_Nr4, col_Eintufung_Nr5=st.columns(2)
-if FG is not "" and AZ is not "" and PS is not "" and DN is not "":
-    Prüfungen_nach_Nr4, Prüfungen_nach_Nr5 = Funktionen_BetrSichV.BetrSichV_RL(float(FG), AZ, float(PS), float(DN))
-    with col_Eintufung_Nr4:
-        st.text_input("Prüfungen nach Nr 4:", Prüfungen_nach_Nr4, disabled=True)
-    with col_Eintufung_Nr5:
-        st.text_input("Prüfungen nach Nr 5:", Prüfungen_nach_Nr5, disabled=True)
+if Rohrleitungen:
+    if FG is not "" and AZ is not "" and PS is not "" and DN is not "":
+        Prüfungen_nach_Nr4, Prüfungen_nach_Nr5 = Funktionen_BetrSichV.BetrSichV_RL(float(FG), AZ, float(PS), float(DN))
+        with col_Eintufung_Nr4:
+            st.text_input("Prüfungen nach Nr 4:", Prüfungen_nach_Nr4, disabled=True)
+        with col_Eintufung_Nr5:
+            st.text_input("Prüfungen nach Nr 5:", Prüfungen_nach_Nr5, disabled=True)
+
+#Einstufung Apparate
+if Apparate:
+    if FG is not "" and AZ is not "" and PS is not "" and V is not "":
+        Prüfungen_nach_Nr4, Prüfungen_nach_Nr5 = Funktionen_BetrSichV.BetrSichV_AP(float(FG), AZ, float(PS), float(V))
+        with col_Eintufung_Nr4:
+            st.text_input("Prüfungen nach Nr 4:", Prüfungen_nach_Nr4, disabled=True)
+        with col_Eintufung_Nr5:
+            st.text_input("Prüfungen nach Nr 5:", Prüfungen_nach_Nr5, disabled=True)
 
--- Funktionen_BetrSichV.py
+++ Funktionen_BetrSichV.py
@@ -5,9 +5,6 @@
 
 
 def BetrSichV_RL(FG, AZ, PS, DN):
-    #ggfs. Fluidgruppe durch H-Sätze ersetzen
-    #akut toxisch muss noch ergänzt werden
-
     #Fluidgruppe 2 raus
     if FG==2:
         Pr_Nr4 = "AM"
@@ -73,4 +70,95 @@ def BetrSichV_RL(FG, AZ, PS, DN):
 
 
     return Pr_Nr4, Pr_Nr5
+
+def BetrSichV_AP(FG, AZ, PS, V):
+    #Tabelle3
+    if FG == 1 and AZ == "g":
+        if V>1 and V<=200 and PS>0.5 and PS*V>25 and PS*V<=200:
+            Pr_Nr4 = "bP"
+            Pr_Nr5 = "bP"
+        elif V>200 and PS>0.5 and PS<= 1:
+            Pr_Nr4 = "bP"
+            Pr_Nr5 = "bP"
+        elif V<=1 and PS>200 and PS<=1000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "bP"
+        elif V>1 and PS>1 and PS*V>200 and PS*V<=1000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "bP"
+        elif V<=1 and PS>1000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "ZÜS"
+        elif V>1 and PS>1 and PS*V>1000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "ZÜS"
+        else:
+            Pr_Nr4 = "AM"
+            Pr_Nr5 = "AM"
+
+    #Tabelle 4
+    if FG==2 and AZ=="g":
+        if V>1 and V<=200 and PS>0.5 and PS*V>50 and PS*V<=200:
+            Pr_Nr4 = "bP"
+            Pr_Nr5 = "bP"
+        elif V>200 and PS>0.5 and PS<= 1:
+            Pr_Nr4 = "bP"
+            Pr_Nr5 = "bP"
+        elif V>1 and PS>1 and PS*V>200 and PS*V<=1000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "bP"
+        elif V<=1 and PS>1000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "ZÜS"
+        elif V>1 and PS>1 and PS*V>1000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "ZÜS"
+        else:
+            Pr_Nr4 = "AM"
+            Pr_Nr5 = "AM"
+
+    #tabelle 5
+    if FG == 1 and AZ == "f":
+        if PS>0.5 and PS<=10 and PS*V>200:
+            Pr_Nr4 = "bP"
+            Pr_Nr5 = "bP"
+        elif V<=1 and PS>500 and PS*V<=1000:
+            Pr_Nr4 = "bP"
+            Pr_Nr5 = "bP"
+        elif V<=1 and PS>500 and PS*V>1000 and PS*V<=10000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "bP"
+        elif V>1 and PS>500 and PS*V<=10000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "bP"
+        elif V>1 and PS>10 and PS<=500 and PS*V>200:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "bP"
+        elif PS>500 and PS*V>10000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "ZÜS"
+        else:
+            Pr_Nr4 = "AM"
+            Pr_Nr5 = "AM"
+
+    #tabelle 6
+    if FG == 2 and AZ == "f":
+        if V<=1 and PS>1000 and PS*V<=1000:
+            Pr_Nr4 = "bP"
+            Pr_Nr5 = "bP"
+        elif V<=10 and PS>1000 and PS*V>1000 and PS*V<=10000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "bP"
+        elif PS>10 and PS<=500 and PS*V>10000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "bP"
+        elif PS>500 and PS*V>10000:
+            Pr_Nr4 = "ZÜS"
+            Pr_Nr5 = "ZÜS"
+        else:
+            Pr_Nr4 = "AM"
+            Pr_Nr5 = "AM"
+
+    return Pr_Nr4, Pr_Nr5
+
 
